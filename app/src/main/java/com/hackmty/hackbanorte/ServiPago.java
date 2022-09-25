8dfc4b9f71135e4f2b79dd5172006e9d227c81e7
ANDREA GUERRERO <L18430270@piedrasnegras.tecnm.mx>
2022-09-25 03:20:31 -0500

    Pago Servicios

---
+++ app/src/main/java/com/hackmty/hackbanorte/ServiPago.java
@@ -0,0 +1,14 @@
+package com.hackmty.hackbanorte;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ServiPago extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_servi_pago);
+    }
+}
